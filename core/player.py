a433e6862abade9eb3beb9fc148a9ce6863c9277
Kitch1234 <107788209+Kitch1234@users.noreply.github.com>
2026-07-21 01:45:34 +0300

    Update player.py

--- core/player.py
+++ core/player.py
@@ -2,61 +2,41 @@ from core.database import db
 
 
 async def create_player(discord_id, username):
+
     query = """
     INSERT INTO users (
         discord_id,
-        username
+        username,
+        level,
+        xp,
+        coins
     )
-    VALUES ($1, $2)
+    VALUES ($1, $2, 1, 0, 100)
     ON CONFLICT (discord_id)
     DO NOTHING;
     """
 
-    await db.execute(
-        query,
-        discord_id,
-        username
-    )
+    async with db.acquire() as conn:
+        await conn.execute(
+            query,
+            discord_id,
+            username
+        )
 
 
 async def get_player(discord_id):
+
     query = """
     SELECT *
     FROM users
     WHERE discord_id = $1;
     """
 
-    player = await db.fetchrow(
-        query,
-        discord_id
-    )
+    async with db.acquire() as conn:
+
+        player = await conn.fetchrow(
+            query,
+            discord_id
+        )
 
     return player
-
-
-async def update_coins(discord_id, amount):
-    query = """
-    UPDATE users
-    SET coins = coins + $1
-    WHERE discord_id = $2;
-    """
-
-    await db.execute(
-        query,
-        amount,
-        discord_id
-    )
-
-
-async def add_xp(discord_id, amount):
-    query = """
-    UPDATE users
-    SET xp = xp + $1
-    WHERE discord_id = $2;
-    """
-
-    await db.execute(
-        query,
-        amount,
-        discord_id
-    )
