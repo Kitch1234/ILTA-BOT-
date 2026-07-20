5e634aff80be640c7393172293d52a76b91340d6
Kitch1234 <107788209+Kitch1234@users.noreply.github.com>
2026-07-20 19:10:17 +0300

    Create connection.py

---
+++ .github/workflows/database/connection.py
@@ -0,0 +1,23 @@
+# connection.py
+
+import asyncpg
+import os
+
+
+DATABASE_CONFIG = {
+
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "database": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD")
+
+}
+
+
+
+async def create_pool():
+
+    return await asyncpg.create_pool(
+        **DATABASE_CONFIG
+    )
