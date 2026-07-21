6f9d407fe8211e170dd68be3da079fc98e3c2186
Kitch1234 <107788209+Kitch1234@users.noreply.github.com>
2026-07-21 11:13:23 +0300

    Update quest.py

--- 123/core/database/quest.py
+++ 123/core/database/quest.py
@@ -10,10 +10,7 @@ async def get_quests(region):
     """
 
     async with db.acquire() as conn:
-        return await conn.fetch(
-            query,
-            region
-        )
+        return await conn.fetch(query, region)
 
 
 async def get_quest(quest_id):
@@ -25,7 +22,37 @@ async def get_quest(quest_id):
     """
 
     async with db.acquire() as conn:
-        return await conn.fetchrow(
+        return await conn.fetchrow(query, quest_id)
+
+
+async def accept_quest(discord_id, quest_id):
+
+    query = """
+    INSERT INTO player_quests (
+        discord_id,
+        quest_id
+    )
+    VALUES ($1, $2);
+    """
+
+    async with db.acquire() as conn:
+        await conn.execute(
             query,
+            discord_id,
             quest_id
+        )
+
+
+async def get_player_quests(discord_id):
+
+    query = """
+    SELECT *
+    FROM player_quests
+    WHERE discord_id = $1;
+    """
+
+    async with db.acquire() as conn:
+        return await conn.fetch(
+            query,
+            discord_id
         )
