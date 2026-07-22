8fcfb1bb7b5994ee951295f89634677337b00b0c
Kitch1234 <107788209+Kitch1234@users.noreply.github.com>
2026-07-22 18:51:16 +0300

    Create  card_layout.py

---
+++ 123/core/data/card_layout.py
@@ -0,0 +1,29 @@
+CARD_LAYOUT = {
+
+    "rarity": (530, 60),
+
+    "number": (880, 105),
+
+    "champion": (530, 962),
+
+    "skin": (530, 1050),
+
+
+    "attack": (300, 1190),
+
+    "health": (580, 1190),
+
+    "defense": (855, 1190),
+
+
+    "region_text": (300, 1350),
+
+    "tier_text": (860, 1350),
+
+
+    "region_icon": (100, 1350),
+
+    "tier_icon": (960, 1350),
+
+    "top_icon": (150, 130)
+}
